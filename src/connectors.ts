0cee37fced5e8665665e7e2b2dedaf6f54928908
narvik.eth <narvik.eth@gmail.com>
2022-02-01 12:26:33 +0900

    add mainnet

--- src/connectors.ts
+++ src/connectors.ts
@@ -2,12 +2,13 @@ import { InjectedConnector } from '@web3-react/injected-connector';
 import { NetworkConnector } from '@web3-react/network-connector';
 
 const RPC_URLS = {
+  1: 'https://mainnet.infura.io/v3/9aa3d95b3bc440fa88ea12eaa4456161',
   80001: 'https://matic-mumbai.chainstacklabs.com',
 };
 
 export const network = new NetworkConnector({
   urls: RPC_URLS,
-  defaultChainId: 80001,
+  defaultChainId: 1,
 });
 
 export const injected = new InjectedConnector({
