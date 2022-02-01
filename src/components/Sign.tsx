c551821ed7d1774148842ee94e945bc0cd3b6842
narvik.eth <narvik.eth@gmail.com>
2022-02-01 18:06:58 +0900

    add jwt sample

--- src/components/Sign.tsx
+++ src/components/Sign.tsx
@@ -9,6 +9,7 @@ export const Sign: React.FC<{ library: Web3Provider }> = ({ library }) => {
   const { account, chainId } = useWeb3React();
   const [sign, setSign] = useState('');
   const [signedAddress, setSignedAddress] = useState('');
+  const [jwt, setJWT] = useState('');
 
   const provider = new ethers.providers.Web3Provider(library.provider);
   const signer = provider.getSigner();
@@ -20,6 +21,16 @@ export const Sign: React.FC<{ library: Web3Provider }> = ({ library }) => {
       setSign(signature);
     });
   }, [signer, message]);
+
+  const handleClaimJWT = useCallback(() => {
+    fetch('http://localhost:8080/claim', {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({ signature: sign, message }),
+    })
+      .then((r) => r.json())
+      .then((obj) => setJWT(obj.jwt));
+  }, [sign, message]);
 
   useEffect(() => {
     if (sign === '') return;
@@ -33,6 +44,24 @@ export const Sign: React.FC<{ library: Web3Provider }> = ({ library }) => {
       <div>address: {account}</div>
       <div>sign: {sign}</div>
       <div>signed address: {signedAddress}</div>
+      <hr />
+      <button onClick={handleClaimJWT}>Claim JWT</button>
+      <div>JWT: {jwt}</div>
+      <div>
+        Parsed:
+        <code>{decodeJwt(jwt)}</code>
+      </div>
     </div>
+  );
+};
+
+const decodeJwt = (token: string) => {
+  if (token === '') return '';
+  const base64Url = token.split('.')[1];
+  const base64 = base64Url.replace(/-/g, '+').replace(/_/g, '/');
+  return JSON.stringify(
+    JSON.parse(decodeURIComponent(window.atob(base64))),
+    null,
+    2,
   );
 };
